d1a3d209243bf56efd1a39934fa6399d3e59bd47
ewewukek <ewewukek@gmail.com>
2021-09-05 15:47:26 +0300

    remove unused method

--- src/main/java/ewewukek/flightmod/FlightMod.java
+++ src/main/java/ewewukek/flightmod/FlightMod.java
@@ -4,8 +4,6 @@ import java.nio.file.Path;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.LivingEntity;
 
 public class FlightMod implements ClientModInitializer {
     public static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("flightmod.txt");
@@ -15,13 +13,5 @@ public class FlightMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         Config.load();
-    }
-
-    public static boolean shouldExecute(LivingEntity entity) {
-        if (!(entity instanceof ClientPlayerEntity)) return false;
-        ClientPlayerEntity player = (ClientPlayerEntity)entity;
-
-        return player.abilities.flying && player.input.pressingForward
-            && (player.input.jumping || player.input.sneaking);
     }
 }
