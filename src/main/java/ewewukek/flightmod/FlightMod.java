18c198a7b90386ea04bd1d33853b13a4e9b86e00
ewewukek <ewewukek@gmail.com>
2021-09-06 01:51:05 +0300

    remove client entrypoint

--- src/main/java/ewewukek/flightmod/FlightMod.java
+++ src/main/java/ewewukek/flightmod/FlightMod.java
@@ -2,16 +2,10 @@ package ewewukek.flightmod;
 
 import java.nio.file.Path;
 
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
-public class FlightMod implements ClientModInitializer {
+public class FlightMod {
     public static final Path CONFIG_ROOT = FabricLoader.getInstance().getConfigDir();
 
     public static boolean overrideVanillaFriction;
-
-    @Override
-    public void onInitializeClient() {
-        Config.setServer(null);
-    }
 }
