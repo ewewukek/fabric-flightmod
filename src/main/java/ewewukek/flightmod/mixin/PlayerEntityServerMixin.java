f80845c7b841620d4ce18819301c10d7a68e4035
ewewukek <ewewukek@gmail.com>
2022-11-06 04:44:05 +0300

    port changes from 1.19

--- src/main/java/ewewukek/flightmod/mixin/PlayerEntityServerMixin.java
+++ src/main/java/ewewukek/flightmod/mixin/PlayerEntityServerMixin.java
@@ -7,23 +7,27 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import ewewukek.flightmod.Config;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tag.FluidTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityServerMixin {
     @Inject(method = "tick", at = @At("HEAD"))
-    public void tick(CallbackInfo ci) {
+    private void tick(CallbackInfo ci) {
         PlayerEntity player = (PlayerEntity)(Object)this;
         PlayerAbilities abilities = player.getAbilities();
 
-        if (player.world.isClient) return;
+        if (player.world.isClient || abilities.invulnerable) return;
 
         if (!Config.enableFlying) {
             if (abilities.allowFlying || abilities.flying) {
-                abilities.allowFlying = false;
-                abilities.flying = false;
+                abilities.allowFlying = abilities.flying = false;
                 player.sendAbilitiesUpdate();
             }
             return;
@@ -33,10 +37,11 @@ public class PlayerEntityServerMixin {
         boolean prevFlying = abilities.flying;
 
         abilities.allowFlying = player.getHungerManager().getFoodLevel() > 0;
-        if (!Config.flyInWater && player.isSubmergedIn(FluidTags.WATER)) {
-            abilities.allowFlying = false;
-        }
-        if (!Config.flyInLava && player.isSubmergedIn(FluidTags.LAVA)) {
+
+        if (!Config.flyInWater && player.isSubmergedIn(FluidTags.WATER)
+        || !Config.flyInLava && player.isSubmergedIn(FluidTags.LAVA)
+        || !Config.flyInSlowBlocks && isTouchingSlowingBlock(player)) {
+
             abilities.allowFlying = false;
         }
 
@@ -52,12 +57,13 @@ public class PlayerEntityServerMixin {
     }
 
     @Inject(method = "increaseTravelMotionStats", at = @At("TAIL"))
-    public void increaseTravelMotionStats(double dx, double dy, double dz, CallbackInfo ci) {
+    private void increaseTravelMotionStats(double dx, double dy, double dz, CallbackInfo ci) {
         PlayerEntity player = (PlayerEntity)(Object)this;
+        PlayerAbilities abilities = player.getAbilities();
 
-        if (player.world.isClient || !Config.enableFlying) return;
+        if (player.world.isClient || abilities.invulnerable) return;
 
-        if (player.getAbilities().flying) {
+        if (abilities.flying) {
             float r = 0.01f * Math.round(100 * (float)Math.sqrt(dx * dx + dz * dz));
             player.addExhaustion(Config.flyingHorizontalCost * r);
             if (dy > 0) {
@@ -66,8 +72,39 @@ public class PlayerEntityServerMixin {
         }
     }
 
-    @Redirect(method = "handleFallDamage", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerAbilities;allowFlying:Z"))
-    public boolean allowFlying(PlayerAbilities abilities) {
-        return !Config.doFallDamage || abilities.creativeMode;
+    private boolean isTouchingSlowingBlock(PlayerEntity player) {
+        if (player.isRegionUnloaded()) {
+            return false;
+        }
+
+        Box box = player.getBoundingBox().contract(0.001);
+        BlockPos.Mutable blockPos = new BlockPos.Mutable();
+
+        for (int x = MathHelper.floor(box.minX); x < MathHelper.ceil(box.maxX); ++x) {
+            for (int y = MathHelper.floor(box.minY); y < MathHelper.ceil(box.maxY); ++y) {
+                for (int z = MathHelper.floor(box.minZ); z < MathHelper.ceil(box.maxZ); ++z) {
+                    blockPos.set(x, y, z);
+                    Block block = player.world.getBlockState(blockPos).getBlock();
+                    if (block == Blocks.COBWEB || block == Blocks.SWEET_BERRY_BUSH || block == Blocks.POWDER_SNOW) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Redirect(
+        method = "handleFallDamage",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/entity/player/PlayerAbilities;allowFlying:Z"
+        )
+    )
+    private boolean handleFallDamagePatch(PlayerAbilities abilities) {
+        if (abilities.invulnerable) { // vanilla behavior
+            return abilities.allowFlying;
+        }
+        return !Config.doFallDamage;
     }
 }
