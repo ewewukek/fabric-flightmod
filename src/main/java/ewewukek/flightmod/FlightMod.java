a50433414772bd0d7b93943738484bef795198cf
ewewukek <ewewukek@gmail.com>
2023-02-19 09:49:04 +0300

    remove /reload command

--- src/main/java/ewewukek/flightmod/FlightMod.java
+++ src/main/java/ewewukek/flightmod/FlightMod.java
@@ -3,12 +3,7 @@ package ewewukek.flightmod;
 import java.nio.file.Path;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.resource.ResourceManager;
-import net.minecraft.resource.ResourceType;
-import net.minecraft.util.Identifier;
 
 public class FlightMod implements ModInitializer {
     public static final String MODID = "flightmod";
@@ -20,17 +15,5 @@ public class FlightMod implements ModInitializer {
     public void onInitialize() {
         Config.setServer(null);
         Config.load();
-
-        ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
-            @Override
-            public Identifier getFabricId() {
-                return new Identifier(MODID, "reload");
-            }
-
-            @Override
-            public void reload(ResourceManager var1) {
-                Config.load();
-            }
-        });
     }
 }
